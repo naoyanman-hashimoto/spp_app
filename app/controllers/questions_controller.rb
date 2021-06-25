33175f7af8edc9ae7191d891ae2269f58d43b464
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-25 19:33:08 +0900

    課題入力フォームの設定とcreateアクションの定義

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,6 +7,19 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+end
+
+
+private
+
+def question_params
+  params.require(:question).permit(:genre_id, :question_name, :question_content, :tip, :model_answer, :point).merge(user_id: current_user.id)
 end
