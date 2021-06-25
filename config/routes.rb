b2882918482c4ee19ae6db11a34c00a3e88df761
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-25 21:42:14 +0900

    各教科へのパスとビューの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'questions#index'
-  resources :questions, only: [:index, :new, :create]
+  resources :questions, only: [:index, :new, :create] do
+    collection do
+      get 'japanese'
+    end
+    collection do
+      get 'math'
+    end
+    collection do
+      get 'english'
+    end
+  end
 end
