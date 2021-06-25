27a19acbfc2c30da7f278c900b7effe8e7cd2d98
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-25 12:57:41 +0900

    Questionモデルにバリデーションの設定

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,13 @@
 class Question < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :question_name
+    validates :question_content
+    validates :model_answer
+    validates :point,
+               numericality: { only_integer: true, greater_than_or_equal_to: 10,
+                               less_than_or_equal_to: 3000,
+                               message: 'is out of setting range' }
+  end
 end
