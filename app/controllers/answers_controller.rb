4e61180a30ff8574489a7dee41fdba4e52265223
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-29 22:23:33 +0900

    Answerモデルにバリデーションを設定

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,12 +1,14 @@
 class AnswersController < ApplicationController
+  before_action :set_question, only: [:new, :create]
+
   def new
-    @question = Question.find(params[:question_id])
     @answer = Answer.new
   end
 
   def create
     @answer = Answer.new(answer_params)
-    if @answer.save
+    if @answer.valid?
+      @answer.save
       redirect_to root_path
     else
       render :new
@@ -14,6 +16,10 @@ class AnswersController < ApplicationController
   end
 
   private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
 
   def answer_params
     params.require(:answer).permit(:answer_content).merge(user_id: current_user.id, question_id: params[:question_id])
