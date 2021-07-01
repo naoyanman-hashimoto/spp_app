206513ba1ebfbb4575811ad2e763193617fde7f7
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-07-01 15:58:37 +0900

    課題の編集内容を上書き保存できる様に設定

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -26,6 +26,19 @@ class QuestionsController < ApplicationController
     @question = Question.find_by(params[:question_id])
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def question_params
