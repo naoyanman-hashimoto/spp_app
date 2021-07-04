de6b13489854ec68635be88f8d18feac067222e7
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-07-04 12:03:57 +0900

    scoreモデルの単体テストコードの実装

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,7 +2,11 @@ class Score < ApplicationRecord
   belongs_to :user
   belongs_to :answer
 
-  validates :score, presence: true,
-            numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10,
-            message: 'is out of setting range' }
+  with_options presence: true do
+    validates :user
+    validates :answer
+    validates :score,
+              numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10,
+              message: 'is out of setting range' }
+  end
 end
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Score, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      user = FactoryBot.create(:user)
+      answer = FactoryBot.create(:answer)
+      @score = FactoryBot.build(:score, user_id: user.id, answer_id: answer.id)
+      sleep 0.1
+    end
+    describe '回答採点機能' do
+      context '回答採点できる時' do
+        it '必要な情報を適切に入力すると、採点した情報がデータベースに保存されること' do
+          expect(@score).to be_valid
+        end
+      end
+      context '回答採点できない時' do
+        it '点数が空では保存されないこと' do
+          @score.score = ''
+          @score.valid?
+          expect(@score.errors.full_messages).to include("Score can't be blank")
+        end
+        it '点数が10点以上では保存されないこと' do
+          @score.score = '11'
+          @score.valid?
+          expect(@score.errors.full_messages).to include('Score is out of setting range')
+        end
+        it '点数が0点以下では保存されないこと' do
+          @score.score = '-1'
+          @score.valid?
+          expect(@score.errors.full_messages).to include('Score is out of setting range')
+        end
+        it '点数は全角数値では保存されないこと' do
+          @score.score = '１０'
+          @score.valid?
+          expect(@score.errors.full_messages).to include('Score is out of setting range')
+        end
+        it '点数は漢数字では保存されないこと' do
+          @score.score = '十'
+          @score.valid?
+          expect(@score.errors.full_messages).to include('Score is out of setting range')
+        end
+        it '点数は英字では保存されないこと' do
+          @score.score = 'ten'
+          @score.valid?
+          expect(@score.errors.full_messages).to include('Score is out of setting range')
+        end
+        it '点数は半英数字混同では保存できないこと' do
+          @score.score = '1o'
+          @score.valid?
+          expect(@score.errors.full_messages).to include('Score is out of setting range')
+        end
+        it 'user_idが空では保存できないこと' do
+          @score.user_id = nil
+          @score.valid?
+          expect(@score.errors.full_messages).to include("User can't be blank")
+        end
+        it 'answer_idが空では保存できないこと' do
+          @score.answer_id = nil
+          @score.valid?
+          expect(@score.errors.full_messages).to include("Answer can't be blank")
+        end
+      end
+    end
+  end
 end
