e7a5ff764b55dd89a743394d72300d319a1d672e
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-07-05 15:22:32 +0900

    不要な記述の削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,22 +3,4 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @answers = @user.answers.includes(:user)
   end
-
-  # def levelUp
-  #   user = User.find(params[:user_id])
-  #   question = Question.find(params[:question_id])
-  #   totalExp = user.experience_point
-  #   totalExp += question.point
-
-  #   user.experience_point = totalExp
-  #   user.update(experience_point: totalExp)
-  
-  #   levelSetting = LevelSetting.find_by(level: user.level + 1);
-  
-  #   if levelSetting.thresold <= user.experience_point
-  #     user.level = user.level + 1
-  #     user.update(level: user.level)
-  #   end
-  #   redirect_to root_path
-  # end
 end
