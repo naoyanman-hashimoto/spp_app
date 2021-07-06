e3d1b19b142fae22b3e6765dfeb97a8e6ea083fb
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-07-06 14:38:43 +0900

    マイページに次のレベルまでに必要なポイント情報を追加記載

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @answers = @user.answers.includes(:user)
+    @levelSetting = LevelSetting.find_by(level: current_user.level + 1)
   end
 end
