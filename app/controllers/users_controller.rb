14896374fcfe0042536dc37b2b2c5ef5d382f325
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-07-07 01:13:38 +0900

    'gem' chart.jsとgonを使用して経験値指標をグラフ化表現

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @answers = @user.answers.includes(:user)
     @levelSetting = LevelSetting.find_by(level: current_user.level + 1)
+    
+    gon.thresold = @levelSetting.thresold
+    gon.experience_point = @user.experience_point
   end
 end
