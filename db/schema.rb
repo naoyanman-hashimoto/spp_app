39521b1908248b55b25a7e3fc7bc9c8cc51bdf95
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-07-19 11:12:18 +0900

    クワガタ進化用のstag_beetle_evolutionsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_120917) do
+ActiveRecord::Schema.define(version: 2021_07_19_020808) do
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -57,6 +57,13 @@ ActiveRecord::Schema.define(version: 2021_07_05_120917) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["answer_id"], name: "index_scores_on_answer_id"
     t.index ["user_id"], name: "index_scores_on_user_id"
+  end
+
+  create_table "stag_beetle_evolutions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "level", null: false
+    t.string "character_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
